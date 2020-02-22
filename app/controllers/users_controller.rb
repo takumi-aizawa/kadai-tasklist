2acb22a48c6741ccd65fcebfe91e50f0ac46ab26
taku.ai <t.aizawa0910@gmail.com>
2020-02-22 09:55:44 +0000

    トップページでのログイン要求

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,8 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def new
-    @User = User.new
+    @user = User.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root to: "toppages#index"
+  root to: "tasks#index"
+  
+    resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
 end
